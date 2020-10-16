086ff7c9ab2903118832068a1f09bb6132484278
atemmel <temmeladam@gmail.com>
2020-10-16 22:53:22 +0200

    Hmmm

---
+++ cmd/anniversary/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten"
+	"log"
+
+	"github.com/atemmel/anniversary/pkg/common"
+)
+
+func main() {
+	g := common.CreateGame()
+	g.Load("./resources/tilemaps/event.json", 0)
+	ebiten.SetWindowSize(common.WindowWidth, common.WindowHeight)
+	ebiten.SetWindowTitle("Anniversary")
+	ebiten.SetRunnableOnUnfocused(true)
+	ebiten.SetFullscreen(true)
+	ebiten.SetCursorMode(ebiten.CursorModeHidden)
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
+}
